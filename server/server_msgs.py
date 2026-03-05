7789daca3cfce105695dc84090a63b019cacbbba
saarkol <saarkol7@gmail.com>
2026-03-05 16:13:20 +0200

    conv ids and read

--- server/server_msgs.py
+++ server/server_msgs.py
@@ -2,8 +2,9 @@ from typing import Dict
 from typing import List
 
 from DBHandler import DBHandler
-from communication_objects import SendMsg, GetMsg, GenericResponse, MsgResponse
+from communication_objects import SendMsg, GetMsg, GenericResponse, MsgResponse, ReadMsg
 from cookie_handler import CookieHandler
+from collections import defaultdict
 
 
 class ServerMsgs:
@@ -25,30 +26,50 @@ class ServerMsgs:
         sender = cls.db.query(cls.USERS_TABLE, {'email': request.email})
         receiver = cls.db.query(cls.USERS_TABLE, {'email': request.receiver_email})
         if receiver:
-            cls.db.write(cls.MSGS_TABLE,
-                         {'sender_uid': sender[0][0], 'receiver_uid': receiver[0][0], 'subject': request.subject,
-                          'message': request.msg})
+            message_data = {'sender_uid': sender[0][0], 'receiver_uid': receiver[0][0], 'subject': request.subject,
+                            'message': request.msg}
+            if request.reply_to:
+                replied_to = cls.db.query(cls.USERS_TABLE, {'uid': request.reply_to})[0]
+                message_data['conv_uid'] = replied_to[5]
+            else:
+                message_data['conv_uid'] = cls.db.get_max('conv_uid', cls.USERS_TABLE)
+            cls.db.write(cls.MSGS_TABLE, message_data)
             response_data = {'status': True, 'message': 'sent message'}
             return GenericResponse(**response_data)
         response_data = {'status': False, 'message': 'receiver email does not exist'}
         return GenericResponse(**response_data)
 
     @classmethod
-    def get_msgs(cls, request_data: Dict[str, str]) -> List[MsgResponse]:
+    def get_msgs(cls, request_data: Dict[str, str]) -> List[List[MsgResponse]]:
         """
         fetches all the messages to a specific user
         :param request_data: dict of strings in format of GetMsg with the information of the requested user
-        :return: List of MsgResponses with the email information
+        :return: List of Lists of MsgResponses with the email information divided by conv uid
         """
         request = GetMsg(**request_data)
         if not CookieHandler.verify(request.email + request.password, request.cookie):
             return []
-        receiver = cls.db.query(cls.USERS_TABLE, {'email': request.email})
+        receiver = cls.db.query(cls.USERS_TABLE, {'email': request.email, 'read': request.read})
         messages = cls.db.query(cls.MSGS_TABLE, {'receiver_uid': receiver[0][0]})
-        all_messages_data = []
+        conv_uids = defaultdict(list)
         for message in messages:
             sender = cls.db.query(cls.USERS_TABLE, {'uid': message[1]})
-            request_data = {'sender_email': sender[0][1], 'subject': message[3], 'msg': message[4]}
+            request_data = {'uid': message[0], 'sender_email': sender[0][1], 'subject': message[3], 'msg': message[4]}
             message_data = MsgResponse(**request_data)
-            all_messages_data.append(message_data)
-        return all_messages_data
+            conv_uids[message[5]].append(message_data)
+        return list(conv_uids.values())
+
+    @classmethod
+    def read_msg(cls, request_data: Dict[str, str]) -> GenericResponse:
+        """
+        adds a message given in the request_data to the db
+        :param request_data: dict of strings in the format of ReadMsg (only includes the msg uid)
+        :return: in the format of GenericResponse, returns the status of the action
+        """
+        request = ReadMsg(**request_data)
+        if not CookieHandler.verify(request.email + request.password, request.cookie):
+            response_data = {'status': False, 'message': 'you are unauthorized'}
+            return GenericResponse(**response_data)
+        cls.db.update(cls.MSGS_TABLE, {'uid': request.uid}, {'read': True})
+        response_data = {'status': True, 'message': 'message was read'}
+        return GenericResponse(**response_data)
