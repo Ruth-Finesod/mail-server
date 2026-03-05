c641500facad844067a8d6f19eda102c9e5c3abe
saarkol <saarkol7@gmail.com>
2026-03-05 18:25:09 +0200

    debug

--- server/server_msgs.py
+++ server/server_msgs.py
@@ -23,10 +23,10 @@ class ServerMsgs:
         if not CookieHandler.verify(request.email + request.password, request.cookie):
             response_data = {'status': False, 'message': 'you are unauthorized'}
             return GenericResponse(**response_data)
-        sender = cls.db.query(cls.USERS_TABLE, {'email': request.email})
-        receiver = cls.db.query(cls.USERS_TABLE, {'email': request.receiver_email})
+        sender = cls.db.query(cls.USERS_TABLE, {'email': request.email})[0]
+        receiver = cls.db.query(cls.USERS_TABLE, {'email': request.receiver_email})[0]
         if receiver:
-            message_data = {'sender_uid': sender[0][0], 'receiver_uid': receiver[0][0], 'subject': request.subject,
+            message_data = {'sender_uid': sender[0], 'receiver_uid': receiver[0], 'subject': request.subject,
                             'message': request.msg, 'read': False}
             if request.reply_to:
                 replied_to = cls.db.query(cls.USERS_TABLE, {'uid': request.reply_to})[0]
@@ -49,12 +49,15 @@ class ServerMsgs:
         request = GetMsg(**request_data)
         if not CookieHandler.verify(request.email + request.password, request.cookie):
             return []
-        receiver = cls.db.query(cls.USERS_TABLE, {'email': request.email, 'read': request.read})
-        messages = cls.db.query(cls.MSGS_TABLE, {'receiver_uid': receiver[0][0]})
+        receiver = cls.db.query(cls.USERS_TABLE, {'email': request.email})[0]
+        messages_query = {'receiver_uid': receiver[0]}
+        if request.read is False:
+            messages_query['read'] = request.read
+        messages = cls.db.query(cls.MSGS_TABLE, messages_query)
         conv_uids = defaultdict(list)
         for message in messages:
-            sender = cls.db.query(cls.USERS_TABLE, {'uid': message[1]})
-            request_data = {'uid': message[0], 'sender_email': sender[0][1], 'subject': message[3], 'msg': message[4]}
+            sender = cls.db.query(cls.USERS_TABLE, {'uid': message[1]})[0]
+            request_data = {'uid': message[0], 'sender_email': sender[1], 'subject': message[3], 'msg': message[4]}
             message_data = MsgResponse(**request_data)
             conv_uids[message[5]].append(message_data)
         return list(conv_uids.values())
@@ -67,9 +70,6 @@ class ServerMsgs:
         :return: in the format of GenericResponse, returns the status of the action
         """
         request = ReadMsg(**request_data)
-        if not CookieHandler.verify(request.email + request.password, request.cookie):
-            response_data = {'status': False, 'message': 'you are unauthorized'}
-            return GenericResponse(**response_data)
         cls.db.update(cls.MSGS_TABLE, {'uid': request.uid}, {'read': True})
         response_data = {'status': True, 'message': 'message was read'}
         return GenericResponse(**response_data)
