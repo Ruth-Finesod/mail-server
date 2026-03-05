a686d0a4bb2ade79f7d2f1005b2a4cbba1558c3a
ruth-finesod <25ruth25@gmail.com>
2026-03-05 18:26:47 +0200

    f

--- client/msg_handler.py
+++ client/msg_handler.py
@@ -20,13 +20,14 @@ class MsgHandler(BaseClass):
     def _user_parms(self):
         return {'email': self.user.email, 'password': self.user.password, 'cookie': self.user.cookie}
 
-    def send_message(self, reply_to=0):
+    def send_message(self, reply_to=0, receiver='', subject=''):
         """
         send message to the server in SendMsg format
         receive response as GenericResponse and print
         """
-        receiver = input("send message to: ")
-        subject = input("subject of the message: ")
+        if reply_to == 0:
+            receiver = input("send message to: ")
+            subject = input("subject of the message: ")
         message = input("message: ")
         msg = {'receiver_email': receiver, 'subject': subject, 'msg': message, 'reply_to': reply_to}
         body_request = SendMsg(**self._user_parms, **msg)
@@ -40,10 +41,10 @@ class MsgHandler(BaseClass):
         request to receive messages from server in GetMsg format
         receive list of messages as MsgResponse object and print
         """
-        receive_options = {'n': False, 'a':True}
-        new_or_all = input('n: receive only new messages\n a: receive all messages')
+        receive_options = {'n': False, 'a': True}
+        new_or_all = input('n: receive only new messages\n a: receive all messages\nyour choice: ')
         while new_or_all not in receive_options:
-            new_or_all= input('you must choose from the options above: ')
+            new_or_all = input('you must choose from the options above: ')
         body_request = GetMsg(**self._user_parms, read=receive_options[new_or_all])
         request = {ServerMethods.RECEIVE_MESSAGES.value: body_request.model_dump()}
         response = send(request)
@@ -60,9 +61,12 @@ class MsgHandler(BaseClass):
             if msg_num == 'q':
                 return
             else:
+                while not (msg_num.isalnum() and 1 <= int(msg_num) <= len(response)):
+                    msg_num = input('you must choose from the messages above: ')
                 self.read_msg(response[int(msg_num) - 1])
 
-    def print_msg(self, msg):
+    @staticmethod
+    def print_msg(msg):
         send({ServerMethods.READ_MSG.value: ReadMsg(uid=msg.uid)})
         print(f'from: {msg.sender_email}')
         print(f'subject: {msg.subject}')
@@ -75,19 +79,20 @@ class MsgHandler(BaseClass):
             for i, inner_msg in enumerate(msg):
                 print(f'{i + 1}')
                 self.print_msg(inner_msg)
-            next_method = input('q: to return to messages manu\nr: to reply to message\n ')
+            next_method = input('q: to return to messages manu\nr: to reply to message\nyour choice: ')
             if next_method == 'q':
                 return
             if next_method == 'r':
                 msg_num = int(input("enter number of the message to reply to: "))
-                self.send_message(reply_to=msg[msg_num - 1].uid)
+                msg = msg[msg_num - 1]
+                self.send_message(reply_to=msg.uid, receiver=msg.sender_email, subject=f're: {msg.subject}')
 
-        next_method = input('q: to return to messages manu\nr: to reply to message\n ')
+        next_method = input('q: to return to messages manu\nr: to reply to message\nyour choice: ')
         if next_method == 'q':
             return
         if next_method == 'r':
             print(f'replying to {msg.sender_email}')
-            self.send_message(reply_to=msg.uid)
+            self.send_message(reply_to=msg.uid, receiver=msg.sender_email, subject=f're: {msg.subject}')
 
     @staticmethod
     def quit_app():
